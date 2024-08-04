ef44e0c7f64d2dce42cee9a91973ee25928d0af0
zaki-lknr <zaki.hmkc+github@gmail.com>
2024-08-04 13:47:11 +0900

    add: RESTの結果をlocal storageに保存してキャッシュ化

--- sample/rest.js
+++ sample/rest.js
@@ -1,9 +1,29 @@
+function get_data() {
+    const body = localStorage.getItem('rest_response');
+    const users = JSON.parse(body);
+
+    var list = document.getElementById("list");
+
+    for (let e of users) {
+        // console.log(e);
+        // console.log(e.name);
+        var item = document.createElement("li");
+        item.textContent = e.name;
+        list.appendChild(item);
+    }
+}
+
 async function rest() {
     console.log("start")
 
     const res = await fetch("https://jsonplaceholder.typicode.com/users");
-    const users = await res.json();
+    // console.log("response: " + res.status);
+    // const users = await res.json();  // text()を両方実行は不可
+    const body = await res.text();  // json()を両方実行は不可
+    // console.log("body: " + body);
     // console.log(users);
+    localStorage.setItem('rest_response', body);
+    const users = JSON.parse(body);
 
     // users.forEach(function(e) {
     //     console.log(e);
