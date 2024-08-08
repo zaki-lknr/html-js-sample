36d09ecbe914444d01b53f8a84acbeef78673280
zaki-lknr <zaki.hmkc+github@gmail.com>
2024-08-08 22:37:59 +0900

    add: get checkins

--- sample/swarm/swarm.js
+++ sample/swarm/swarm.js
@@ -22,4 +22,29 @@ function load_configure() {
     // update page
     console.log('token: '+ configure.oauth_token);
     document.getElementById("oauth_token").value = configure.oauth_token;
+
+    return configure;
+}
+
+async function get_data() {
+    const configure = load_configure();
+    const url = 'https://api.foursquare.com/v2/users/self/checkins?v=20231010&limit=30&offset=0&oauth_token=' + configure.oauth_token;
+    // console.log('url: ' + url);
+    const headers = new Headers();
+    headers.append('accept', 'application/json');
+
+    const res = await fetch(url, { headers: headers });
+
+    const body = await res.text();
+    // console.log('body: ' + body);
+
+    localStorage.setItem('rest_response', body);
+}
+
+function load_data() {
+    const checkins = localStorage.getItem('rest_response');
+    // console.log('checkins: ' + checkins);
+    const checkin_data = JSON.parse(checkins);
+    // console.log('checkin_data: ' + checkin_data);
+
 }
