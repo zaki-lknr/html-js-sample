9ee217dcf15f735f37401934791d55c2f9a3b4da
zaki-lknr <zaki.hmkc+github@gmail.com>
2024-08-21 21:35:44 +0900

    add: https://corsproxy.io/を使った任意のサイトの情報取得

---
+++ sample/ogp.js
@@ -0,0 +1,26 @@
+async function get_ogp() {
+    console.log("start")
+
+    const url = document.getElementById("sample_url").value;
+    // console.log(url);
+    // if (url == null) {
+    //     console.log("null");
+    // }
+    // if (url.startsWith('http')) {
+    //     console.log("start http");
+    // }
+    // console.log(url.length);
+    const proxy_url = 'https://corsproxy.io/?' + encodeURIComponent(url);
+
+    // const headers = new Headers();
+    // headers.append('Access-Control-Allow-Origin', '*');
+
+    // const res = await fetch(url, { method: "GET", headers: headers, mode: 'cors' });
+    const res = await fetch(proxy_url);
+    const t = await res.text();
+    const d = new DOMParser().parseFromString(t, "text/html");
+
+    console.log(d);
+    console.log(d.head);
+    console.log(d.title);   // title
+}
