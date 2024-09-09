29bb98a014fb367e33688c728e92ea4e3d92a163
zaki-lknr <zaki.hmkc+github@gmail.com>
2024-09-09 22:08:00 +0900

    add: query stringの値取り出し

--- sample/query.js
+++ sample/query.js
@@ -2,4 +2,17 @@ async function exec() {
     console.log("start");
 
     console.log(window.location.search);
+    if (window.location.search.length > 0) {
+        const param = new URLSearchParams(window.location.search);
+        console.log(param);
+        // console.log(param.keys());
+        for (const key of param.keys()) {
+            console.log(key);
+            console.log(param.get(key));
+        }
+        for (const [key, val] of param) {
+            console.log(key);
+            console.log(val);
+        }
+    }
 }
