1c0bea080dd5cddc9402bbfb425bd8dd46869e89
zaki-lknr <zaki.hmkc+github@gmail.com>
2024-10-07 21:02:46 +0900

    add: クリップボードからのペーストsample

--- sample/clipboard.js
+++ sample/clipboard.js
@@ -13,3 +13,23 @@ function clipboard() {
         document.execCommand('copy');
     }
 }
+
+
+function paste() {
+    const input_text = document.getElementById("input_text");
+    console.log("input_text: " + input_text);
+
+    if (navigator.clipboard) {
+        navigator.clipboard.readText().then((clipText) => {
+            console.log(clipText);
+            input_text.value = clipText;
+        })
+    }
+    else {
+        // HTTP経由だとnavigator.clipboardが機能しない
+        console.log("document write");
+        input_text.focus();
+        document.execCommand("paste");
+        // ただ、↑でも動かない
+    }
+}
