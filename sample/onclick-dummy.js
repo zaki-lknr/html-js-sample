4f7868621321f37914f70189bca20e4877cd6263
zaki-lknr <zaki.hmkc+github@gmail.com>
2025-05-03 16:27:06 +0900

    add: 動的生成したhtmlからのonclickお試し

--- sample/onclick-dummy.js
+++ sample/onclick-dummy.js
@@ -3,4 +3,11 @@
  */
 const hello_world = () => {
     console.log("hello_world");
+
+    const v = document.getElementById('view');
+    v.innerHTML = 'hello <span onclick="press_here()">press here</span>';
+}
+
+const press_here = () => {
+    console.log("pressed here.");
 }
