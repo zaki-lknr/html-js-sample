7e7f54e9e51fa9dd9bccffec61272ddad7665594
zaki-lknr <zaki.hmkc+github@gmail.com>
2025-05-03 17:55:35 +0900

    add: 外部jsの読み込みをDOM操作で実施

---
+++ sample/onclick-dummy-library.js
@@ -0,0 +1,15 @@
+// export class Person {
+class Person {
+    name;
+    age;
+
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    hello() {
+        console.log('hello, my name is ' + this.name + '.');
+        alert('hello, my name is ' + this.name + '.');
+    }
+}
--- sample/onclick-dummy.js
+++ sample/onclick-dummy.js
@@ -1,6 +1,18 @@
 /*
  * write your code
  */
+
+// import {Person} from "./onclick-dummy-library.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const elm = document.createElement('script');
+    elm.src = 'onclick-dummy-library.js';
+    // elm.type = 'module';
+    // console.log(elm);
+    // console.log(document.body);
+    document.body.appendChild(elm);
+});
+
 const hello_world = () => {
     console.log("hello_world");
 
@@ -10,4 +22,6 @@ const hello_world = () => {
 
 const press_here = () => {
     console.log("pressed here.");
+    const p = new Person("aaa", 17);
+    p.hello();
 }
